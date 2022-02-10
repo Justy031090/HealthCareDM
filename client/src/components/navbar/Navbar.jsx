81ed35c926146eddc0d45c6e06c540845d1358ee
justy031090 <justy031090@gmail.com>
2022-02-10 04:10:34 +0200

    user menu dropdown added

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -6,52 +6,69 @@ import { logout } from '../../actions/loginAction';
 
 const Navbar = () => {
     const [calcDropdown, setCalcDropdown] = useState(false);
+    const [userDropdown, setUserDropdown] = useState(false);
     const userLogin = useSelector((state) => state.userLogin);
     const { userInfo } = userLogin;
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const ref = useRef();
+    const calcRef = useRef();
+    const userRef = useRef();
 
     const logoutHandler = () => {
         dispatch(logout());
         navigate('/');
     };
-    const handleMouseDown = () => {
+    const handleCalcMouseDown = () => {
         setCalcDropdown(false);
     };
-    const timeout = setTimeout(handleMouseDown, 2500);
 
-    const handleMouseEnter = () => {
-        clearTimeout(timeout);
+    const handleCalcMouseEnter = () => {
         setCalcDropdown(true);
+        setTimeout(handleCalcMouseDown, 4000);
+    };
+    const handleUserMouseEnter = () => {
+        setUserDropdown(true);
     };
     useEffect(() => {
-        let handler = (e) => {
-            if (!ref.current.contains(e.target)) {
+        let calcHandler = (e) => {
+            if (!calcRef.current.contains(e.target)) {
                 setCalcDropdown(false);
             }
         };
-        document.addEventListener('mousedown', handler);
+        document.addEventListener('mousedown', calcHandler);
 
         return () => {
-            document.removeEventListener('mousedown', handler);
+            document.removeEventListener('mousedown', calcHandler);
         };
     }, [calcDropdown]);
 
+    useEffect(() => {
+        let userHandler = (e) => {
+            if (!userRef.current.contains(e.target)) {
+                setUserDropdown(false);
+            }
+        };
+        document.addEventListener('mousedown', userHandler);
+
+        return () => {
+            document.removeEventListener('mousedown', userHandler);
+        };
+    }, [userDropdown]);
+
     return (
-        <nav className="navbar container">
-            <p className="navbar logo">m.Care</p>
+        <nav className="navbar-container">
+            <div className="logo">m.Care</div>
             <Link to="/">Home</Link>
             <Link to="/recipies">Recipies</Link>
             <div
                 className="dropdown"
-                ref={ref}
-                onClick={(e) => setCalcDropdown(false)}
+                ref={calcRef}
+                onClick={() => setCalcDropdown(false)}
             >
                 <button
                     className="btn btn-primary"
-                    onMouseEnter={() => handleMouseEnter()}
-                    onMouseLeave={() => timeout()}
+                    onClick={() => setCalcDropdown(true)}
+                    onMouseOver={() => handleCalcMouseEnter()}
                 >
                     Calculators <i className="fa fa-caret-down"></i>
                 </button>
@@ -62,24 +79,45 @@ const Navbar = () => {
                             : 'dropdown-content'
                     }
                 >
-                    <div>
-                        <Link to="/calculators/bolus">Bolus Calculator</Link>
-                        <Link to="/recipies">Recipies</Link>
-                    </div>
+                    <Link to="/calculators/bolus">Bolus Calculator</Link>
+                    <Link to="/calculators/bmi">BMI Calculator</Link>
                 </div>
             </div>
             <Link to="/forum">Forum</Link>
 
             {userInfo ? (
-                <div className="user-info">
-                    <div className="logout" onClick={logoutHandler}>
+                <div className={'user-info'} ref={userRef}>
+                    <button
+                        className="btn btn-primary"
+                        onClick={() => setUserDropdown(true)}
+                        onMouseOver={() => handleUserMouseEnter()}
+                    >
+                        <i className="fas fa-user"></i>{' '}
+                        {userInfo.firstName[0].toUpperCase() +
+                            userInfo.firstName.substring(1)}{' '}
+                        <i className="fa fa-caret-down"></i>
+                    </button>
+                    <div
+                        className={
+                            userDropdown ? 'user-content show' : 'user-content'
+                        }
+                    >
+                        <button
+                            className="btn btn-primary"
+                            onClick={logoutHandler}
+                        >
+                            Logout
+                        </button>
+                        <Link to="/profile">Profile</Link>
+                    </div>
+                    {/* <div className="logout" onClick={logoutHandler}>
                         {userInfo.firstName[0].toUpperCase() +
                             userInfo.firstName.substring(1)}
                         <small className="small">Logout(dropdown)</small>
-                    </div>
-                    <div className="profile">
+                    </div> */}
+                    {/* <div className="profile">
                         <Link to="/profile">Profile</Link>
-                    </div>
+                    </div> */}
                 </div>
             ) : (
                 <div className="login" onClick={() => navigate('/login')}>
